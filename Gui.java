3ea9788bd351600fbdb47c4adf7cbb533af9cd1f
Calvin-Glein <glenn_matias@dlsu.edu.ph>
2016-11-08 00:23:05 +0800

    Add files via upload

--- Gui.java
+++ Gui.java
@@ -30,9 +30,12 @@ import javax.swing.border.MatteBorder;
 
 import java.awt.GridLayout;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.SwingConstants;
 
 public class Gui extends JFrame implements ActionListener {
 
+	int boardRow = 4;
+	int boardCol = 8;
 	JPanel leftPanel = new JPanel();
 	JPanel rightPanel = new JPanel();
 
@@ -48,20 +51,49 @@ public class Gui extends JFrame implements ActionListener {
 		}
 	};
 
-	JTable table = new JTable(model);
+	JTable table;
 	private final JLabel lblBattlesheepDashboard = new JLabel("Battlesheep dashboard:");
-	private final JLabel lblTotalSheepPer = new JLabel("Total sheep per player");
+	private final JLabel lblTotalSheepPerPlayer = new JLabel("Total sheep per player:");
 	private final JLabel totalSheep = new JLabel("n");
+	private final JLabel lblPlayerDashboard = new JLabel("Player dashboard");
+	private final JLabel lblSheepAtHandLabel = new JLabel("Sheep at hand:");
+	private final JLabel lblSheepAtHand = new JLabel("n");
+	private final JLabel lblFromXCoordinate = new JLabel("From x coordinate:");
+	private final JLabel lblFromYCoordinate = new JLabel("From y coordinate:");
+	private final JLabel lblxCoordinate = new JLabel("x");
+	private final JLabel lblyCoordinate = new JLabel("y");
+	private final JButton btnSubmit = new JButton("Done");
+
+	int numberOfSheepsPerPlayer = 0;
+	Boolean isHolding = 0;
 
 	public Gui() {
-		rightPanel.setLayout(new MigLayout("", "[][]", "[][]"));
+		rightPanel.setLayout(new MigLayout("", "[185.00][33.00]", "[][][][][][][][][][][][][][][]"));
 		lblBattlesheepDashboard.setFont(new Font("Tahoma", Font.BOLD, 13));
-		
+
 		rightPanel.add(lblBattlesheepDashboard, "cell 0 0");
-		
-		rightPanel.add(lblTotalSheepPer, "cell 0 1");
-		
+
+		rightPanel.add(lblTotalSheepPerPlayer, "cell 0 1");
+
 		rightPanel.add(totalSheep, "cell 1 1,aligny baseline");
+		lblPlayerDashboard.setFont(new Font("Tahoma", Font.BOLD, 12));
+
+		rightPanel.add(lblPlayerDashboard, "cell 0 3");
+
+		rightPanel.add(lblSheepAtHandLabel, "cell 0 4");
+
+		rightPanel.add(lblSheepAtHand, "cell 1 4,aligny baseline");
+
+		rightPanel.add(lblFromXCoordinate, "cell 0 5");
+
+		rightPanel.add(lblxCoordinate, "cell 1 5,alignx left");
+
+		rightPanel.add(lblFromYCoordinate, "cell 0 6");
+
+		rightPanel.add(lblyCoordinate, "cell 1 6");
+		btnSubmit.setVerticalAlignment(SwingConstants.BOTTOM);
+
+		rightPanel.add(btnSubmit, "cell 0 14,growx");
 
 		this.setSize(1000, 700);
 
@@ -79,48 +111,60 @@ public class Gui extends JFrame implements ActionListener {
 		splitPane.setVisible(true);
 		splitPane.setDividerLocation(750);
 
-		String tite = JOptionPane.showInputDialog(this, "Enter your message", "Messages", 2);
-		
-	
-
 		initializeTable();
+		initializeTableLooks();
+
+		// ask number sheeps per player store it in numberOfSheepsPerPlayer
+		numberOfSheepsPerPlayer = Integer
+				.parseInt(JOptionPane.showInputDialog(this, "Total number of sheeps per player", "Welcome", 2));
+		totalSheep.setText(Integer.toString(numberOfSheepsPerPlayer));
+
+		// ask where player wants to put all his sheep (where to put stack)
+
+		model.setValueAt(Integer.toString(numberOfSheepsPerPlayer),
+				Integer.parseInt(JOptionPane.showInputDialog(this, "X coordinate:",
+						"Where do you want to put all your sheep?", 2)),
+				Integer.parseInt(JOptionPane.showInputDialog(this, "Y coordinate:",
+						"Where do you want to put all your sheep?", 2)));
 
 		leftPanel.add(table);
+
+	}
+
+	public void initializeTableLooks() {
+		table.setRowHeight(45);
+		TableColumn column = null;
+
+		for (int i = 0; i < 8; i++) {
+			column = table.getColumnModel().getColumn(i);
+			column.setPreferredWidth(45);
+		}
+		for (int i = 0; i < 8; i++) {
+			DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+			centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+			table.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+		}
 
 	}
 
 	public void initializeTable() {
 		// <----><----><----><---->---------table parts
 
-		table.setRowHeight(30);
-
-		for (int i = 0; i < 15; i++) {
+		for (int i = 0; i < boardCol; i++) {
 			model.addColumn("*");
 		}
 
-		TableColumn column = null;
-
-		for (int i = 0; i < 15; i++) {
-			column = table.getColumnModel().getColumn(i);
-			column.setPreferredWidth(50);
-		}
-
-		for (int j = 0; j < 20; j++) {
+		for (int j = 0; j < boardRow * 2; j++) {
 			if (j % 2 == 0)
-				model.addRow(new Object[] { "0", "<---->", "0", "<---->", "0", "<---->", "0", "<---->", "0", "<---->",
-						"0", "<---->", "0", "<---->", "0" });
+				model.addRow(new Object[] { "0", "<---->", "0", "<---->", "0", "<---->", "0", "|||||" });
 			else
-				model.addRow(new Object[] { "|", "0", "<---->", "0", "<---->", "0", "<---->", "0", "<---->", "0",
-						"<---->", "0", "<---->", "0", "|" });
+				model.addRow(new Object[] { "|||||", "0", "<---->", "0", "<---->", "0", "<---->", "0" });
 		}
 
 		// center all data in table
 
-		for (int i = 0; i < 15; i++) {
-			DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-			centerRenderer.setHorizontalAlignment(JLabel.CENTER);
-			table.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
-		}
+		table = new JTable(model);
+
 	}
 
 	@Override
